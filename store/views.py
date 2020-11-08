e93568892d5a5ef02f004e7b8dab4e6d9ba5eedf
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-11-08 21:12:58 +0530

    models

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import  HttpResponse
 # Create your views here.
+from .models.product import Product
+
+def index(request):
+    prds =Product.get_all_products();
+    return render(request,'index.html',{'products':prds})
