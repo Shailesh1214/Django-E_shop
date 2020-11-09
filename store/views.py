62013c20aa43d8557fffa72f5659d22e0728aee8
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-11-09 16:22:11 +0530

    filter products

--- store/views.py
+++ store/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 # Create your views here.
 from .models.product import Product
+from .models.category import Category
 
 def index(request):
-    prds =Product.get_all_products();
-    return render(request,'index.html',{'products':prds})
+    products = Product.get_all_products();
+    categories = Category.get_all_categories()
+    categoryID = (request.GET.get('category'))
+    if categoryID:
+        products =Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request,'index.html', data)
