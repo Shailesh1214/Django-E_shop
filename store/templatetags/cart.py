b7c23f5d94e21255ed97ee872c5aa9ab700eeb3d
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-11-11 15:52:11 +0530

    cart

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -4,10 +4,19 @@ register = template.Library()
 
 # cart lgc
 @register.filter(name='is_in_cart')
+def is_in_cart(product,cart):
+    keys = cart.keys()
+    for id in keys:
+        if id == str(product.id):
+            
+            return True
+    return False
+
+@register.filter(name='cart_quantity')
 def is_in_cart(Product,cart):
     keys = cart.keys()
     for id in keys:
         if id== str(Product.id):
             
-            return True
-    return False
+            return cart.get(id)
+    return 0
