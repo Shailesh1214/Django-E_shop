daac1f2f7cef952e4170c837a7427c9cfcb189cf
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-11-11 17:24:29 +0530

    logout

--- store/views/login.py
+++ store/views/login.py
@@ -15,7 +15,7 @@ class Login(View):
         if customer:
             flag = check_password(password, customer.password)
             if flag:
-                request.session['customer_id'] = customer.id
+                request.session['customer'] = customer.id
                 request.session['email'] = customer.email
                 return redirect('homepage')
             else:
@@ -25,3 +25,7 @@ class Login(View):
         print(customer)
         print(email, password)
         return render(request,'Login.html',{'error':error_message})
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
