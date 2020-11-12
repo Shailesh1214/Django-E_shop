98e20edef3de4b7cf736b5e6fb7010bf630a936c
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-11-12 20:50:23 +0530

    Return Url & Middleware

--- store/views/login.py
+++ store/views/login.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import check_password
+from django.shortcuts import render , redirect , HttpResponseRedirect
+
+from django.contrib.auth.hashers import  check_password
 from store.models.customer import Customer
-from django.views import View
+from django.views import  View
+
 
 class Login(View):
-    def get(self,request):
-        return render(request, 'Login.html')
+    return_url = None
+    def get(self , request):
+        Login.return_url = request.GET.get('return_url')
+        return render(request , 'login.html')
 
-    def post(self,request):
+    def post(self , request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         customer = Customer.get_customer_by_email(email)
@@ -16,15 +20,19 @@ class Login(View):
             flag = check_password(password, customer.password)
             if flag:
                 request.session['customer'] = customer.id
-                request.session['email'] = customer.email
-                return redirect('homepage')
+
+                if Login.return_url:
+                    return HttpResponseRedirect(Login.return_url)
+                else:
+                    Login.return_url = None
+                    return redirect('homepage')
             else:
-                error_message ='Email or password invalid !!'
+                error_message = 'Email or Password invalid !!'
         else:
-            error_message ='Email or password invalid !!'
-        print(customer)
+            error_message = 'Email or Password invalid !!'
+
         print(email, password)
-        return render(request,'Login.html',{'error':error_message})
+        return render(request, 'login.html', {'error': error_message})
 
 def logout(request):
     request.session.clear()
